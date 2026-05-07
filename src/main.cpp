3bd6dc13946853f0b3a215722bd8188558e3eab1
Lily Wang <lilywang@Lilys-MacBook-Pro-4.local>
2026-05-07 16:51:39 -0500

    Initial project scaffold

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Decision tree project starting..." << std::endl;
+    return 0;
+}
---
+++ tests/test_tree.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Tests passing!" << std::endl;
+    return 0;
+}
